5e8f8ee2dec535e614ed1a93ad9483d5a2b80d9f
Dylan Littlewood <dylanlittlewood@live.co.uk>
2024-09-13 22:04:46 +0100

    Updated Title and message on page

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-          <h1>Initial App</h1>
+          <h1>Welcome to Dylan and Jess' wedding website</h1>
 
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
